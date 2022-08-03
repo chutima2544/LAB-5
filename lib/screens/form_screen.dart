054db985aff7fa279f362f4923f0dbe876119a6a
chutima <chutimamaa2544@gmail.com>
2022-08-03 15:42:02 +0700

    form

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -7,6 +7,35 @@ class FormScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("การสั่งซื้อ"),
         ),
-        body: Container());
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField( 
+                  decoration: new InputDecoration(labelText: "ชื่อสินค้า"),
+                  autofocus: true,
+                ),
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "จำนวนสินค้า"),
+                  keyboardType: TextInputType.number,
+                ),
+                TextFormField(
+                  decoration: new InputDecoration(labelText: "ราคาสินค้า"),
+                  keyboardType: TextInputType.number,
+                ),
+                FlatButton(
+                  child: Text("เพิ่มข้อมูล"),
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  onPressed: (){
+                    Navigator.pop(context);
+                  }, 
+                  )
+              ],
+            ),
+          ),
+        ));
   }
 }
