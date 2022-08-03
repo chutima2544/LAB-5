345e9c534d3ffb866fb43800101954b94598cab7
chutima <chutimamaa2544@gmail.com>
2022-08-03 21:06:12 +0700

    Provider

--- lib/providers/information_provider.dart
+++ lib/providers/information_provider.dart
@@ -3,17 +3,15 @@ import 'package:flutter_application_o2/models/Information.dart';
 
 class InformationProvider with ChangeNotifier{
     
-      List<Information> informations = [
-        Information(title: "ดินสอ",product: 100,price: 60),
-        Information(title: "สมุด",product: 50,price: 10),
-        Information(title: "หนังสือ",product: 60,price: 45)
-      ];
+      List<Information> informations = [];
       
       List<Information> getInformation(){
         return informations;
       }
 
       void addInformation(Information statement){
-          informations.add(statement);
+          informations.insert(0,statement);
+
+          notifyListeners();
       }
 }
--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_o2/models/Information.dart';
+import 'package:flutter_application_o2/providers/information_provider.dart';
+import 'package:provider/provider.dart';
 
 class FormScreen extends StatelessWidget {
   final formKey = GlobalKey<FormState>();
@@ -65,12 +68,16 @@ class FormScreen extends StatelessWidget {
                     if(formKey.currentState!.validate()){   
                       var title = titleController.text;
                       var product = productController.text;  
-                      var price = priceController.text;     
+                      var price = priceController.text;  
 
-                      print(title);
-                      print(product);
-                      print(price); 
-                      
+                      Information statement = Information(
+                        title:title,
+                        product: double.parse(product),
+                        price: double.parse(price),
+                      );   
+
+                      var provider = Provider.of<InformationProvider>(context,listen:false);
+                      provider.addInformation(statement);
                       Navigator.pop(context);
                     }
                   }, 
