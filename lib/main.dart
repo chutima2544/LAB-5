411038e0c4b53c47101727eb0f5573242c4d0553
chutima <chutimamaa2544@gmail.com>
2022-08-03 14:57:18 +0700

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -32,17 +32,18 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-          icon: Icon(Icons.add), 
-          onPressed:(){
-
-          })
-        ],
-      ),
-      body: Container()
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Text("สมุด");
+                  }));
+                })
+          ],
+        ),
+        body: Container());
   }
 }
