e23290fba641409fc52eca2c1d16c4469f4328d7
chutima <chutimamaa2544@gmail.com>
2022-08-03 20:35:57 +0700

    validation

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,12 +11,19 @@ class FormScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 TextFormField( 
                   decoration: new InputDecoration(labelText: "ชื่อสินค้า"),
                   autofocus: true,
+                  validator: (String? str){
+                      if(str!.isEmpty){
+                          return "กรุณาป้อนรายการ";
+                      }
+                      return null;
+                  },
                 ),
                 TextFormField(
                   decoration: new InputDecoration(labelText: "จำนวนสินค้า"),
@@ -24,13 +32,24 @@ class FormScreen extends StatelessWidget {
                 TextFormField(
                   decoration: new InputDecoration(labelText: "ราคาสินค้า"),
                   keyboardType: TextInputType.number,
+                  validator: (String? str){
+                    if(str!.isEmpty){
+                          return "กรุณาป้อนราคาสินค้า";
+                      }
+                      if(double.parse(str)<=0){
+                        return "กรุณาป้อนตัวเลขมากกว่า 0";
+                      }
+                      return null;
+                  },
                 ),
                 FlatButton(
                   child: Text("เพิ่มข้อมูล"),
                   color: Colors.pink,
                   textColor: Colors.white,
                   onPressed: (){
-                    Navigator.pop(context);
+                    if(formKey.currentState!.validate()){         
+                      Navigator.pop(context);
+                    }
                   }, 
                   )
               ],
